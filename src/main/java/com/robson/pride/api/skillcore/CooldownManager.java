9104da9052f5192f72beaf2894d099e2e6bcfbb0
robson <hapoeno@gmail.com>
2025-05-13 21:27:36 -0300

    intial

---
+++ src/main/java/com/robson/pride/api/skillcore/CooldownManager.java
@@ -0,0 +1,24 @@
+package com.robson.pride.api.skillcore;
+
+import net.minecraft.world.entity.player.Player;
+import yesman.epicfight.api.animation.Joint;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CooldownManager {
+
+    public static ConcurrentHashMap<Player, ConcurrentHashMap<String, Short>> playerSkillsCooldown = new ConcurrentHashMap<>();
+
+    public static void timeCooldowns(Player player){
+        if (player != null && playerSkillsCooldown.get(player) != null){
+            for (String entry : playerSkillsCooldown.get(player).keySet()){
+                short value = playerSkillsCooldown.get(player).get(entry);
+                playerSkillsCooldown.get(player).put(entry, (short) (value - 1));
+                if (value <= 0){
+                    playerSkillsCooldown.get(player).remove(entry);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/robson/pride/registries/StructureRegister.java
@@ -0,0 +1,17 @@
+package com.robson.pride.registries;
+
+import net.minecraft.sounds.Music;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.phys.AABB;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StructureRegister {
+
+    public static Map<AABB, String> structuresMap = new HashMap<>();
+
+    public static void register(){
+
+    }
+}
